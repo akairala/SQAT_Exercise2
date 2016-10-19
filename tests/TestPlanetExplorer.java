48393eae3269143c97124a57121a3aec83a93814
somename <someemail>
2016-10-19 15:17:26 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -4,8 +4,26 @@ import org.junit.Test;
 
 public class TestPlanetExplorer {
 
+	// Create a new PlanetExplorer for tests
+	PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)");
+	
 	@Test
-	public void test_executeCommand() {
+	public void test_getInitialPosition() {
+			
+		// Get value
+		String position = explorer.getPosition();
+		
+		// Assert
+		assertEquals("Invalid initial position", 0, 0, position);
+	}
+	
+	@Test
+	public void test_getDirection() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test_moveForward() {
 		fail("Not yet implemented");
 	}
 }
